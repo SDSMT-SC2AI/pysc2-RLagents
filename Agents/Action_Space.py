d6ee2a49d34774db9d3698a9e04265e6a67366a3
Mathias <Mathias.Wingert@mines.sdsmt.edu>
2018-01-26 22:51:03 -0700

    Fixed the Return values

--- Agents/Action_Space.py
+++ Agents/Action_Space.py
@@ -3,25 +3,26 @@ class Action_Space:
 
     #the unit ID
     id;
-    valid_units = [(126, ("move", "inject_larva")), #queen
-                   (104, ("move", "build_hatchery", "build_spawning_pool")), #drone
-                   (151, ("morph_drone", "morph_overlord")), #larva
-                   (86), ("build_queen")]  # hatchery
+
 
     def __init__(self):
         return
     def get_actions(self, unit_id):
         id = unit_id
+        valid_units = [(126, ("move", "inject_larva")), #queen
+               (104, ("move", "build_hatchery", "build_spawning_pool")), #drone
+               (151, ("morph_drone", "morph_overlord")), #larva
+               (86), ("build_queen")]  # hatchery
         #compare the ID provided with the allowed IDS
-        for id in valid_units:
-            if(id == valid_units[0]):
+        for x in range(4):
+            if(id == valid_units[x][0]):
                 #If an ID is mathced return the corrasponding action vector
-                return valid_units[1]
+                return valid_units[x][1]
            
 # testing and debug function
 def main():
     t1 = Action_Space
-    print (t1.get_actions(t1, 126))
+    print (t1.get_actions(t1, 151))
 
 if __name__ == '__main__':
 	main()
