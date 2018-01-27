347abc1df80285c0cd48fea6a9808cfcb20216d0
Mathias <Mathias.Wingert@mines.sdsmt.edu>
2018-01-27 10:53:25 -0700

    A few quality changes and filled in the action vector with the actions found in actions.py.

--- Agents/Action_Space.py
+++ Agents/Action_Space.py
@@ -1,20 +1,19 @@
 
 class Action_Space:
-
-    #the unit ID
-    id;
-
-
     def __init__(self):
         return
+
+    #compare the provided ID number to the ID of the units allowed
     def get_actions(self, unit_id):
         id = unit_id
-        valid_units = [(126, ("move", "inject_larva")), #queen
-               (104, ("move", "build_hatchery", "build_spawning_pool")), #drone
-               (151, ("morph_drone", "morph_overlord")), #larva
-               (86), ("build_queen")]  # hatchery
+        #define the units and their actions
+        valid_units = [(126, ("Move_screen", "Effect_InjectLarva_screen")), #queen
+               (104, ("Move_screen", "Harvest_Gather_screen", "Build_Hatchery_screen", "Build_SpawningPool_screen")), #drone
+               (151, ("Train_Drone_quick", "Train_Overlord_quick")), #larva
+               (86), ("Train_Queen_quick")]  # hatchery
+        num_units = len(valid_units)
         #compare the ID provided with the allowed IDS
-        for x in range(4):
+        for x in range(num_units):
             if(id == valid_units[x][0]):
                 #If an ID is mathced return the corrasponding action vector
                 return valid_units[x][1]
